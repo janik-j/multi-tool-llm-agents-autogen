a260719a419ecec76d34672c1ac9b1ec64c384a9
Nikita Kostin <nikita.kostin@tum.de>
2024-06-19 00:02:08 +0200

    Initial implementation of a PDF parser

---
+++ demo/pages/7_PDF_Triage.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import tempfile
+from autogen.io.base import IOStream
+
+from demo import IOStreamlitChat
+from wrappers import PdfTriageWrapper
+
+
+IOStream.set_global_default(IOStreamlitChat())
+
+st.title("AutoGen PDF Triage")
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key")
+openai_model = st.sidebar.selectbox("OpenAI Model", ["gpt-3.5-turbo", "gpt-4", "gpt-4o"])
+
+config_list = [{"model": openai_model, "api_key": openai_api_key}]
+
+
+with st.form("pdf_triage"):
+    question = st.text_input("Enter your question")
+    uploaded_file = st.file_uploader("Add attachment", type=["pdf"])
+
+    submitted = st.form_submit_button("Generate an answer")
+
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+
+    if submitted and openai_api_key.startswith("sk-"):
+        if uploaded_file is None:
+            pdf_triage_wrapper = PdfTriageWrapper(config_list, pdf_path=None)
+            pdf_triage_wrapper.initiate_chat(question)
+        else:
+            with tempfile.NamedTemporaryFile(mode="wb") as f:
+                f.write(uploaded_file.read())
+                pdf_triage_wrapper = PdfTriageWrapper(config_list, pdf_path=f.name)
+                pdf_triage_wrapper.initiate_chat(question)
---
+++ wrappers/pdf_triage_wrapper.py
@@ -0,0 +1,109 @@
+import openai
+import pymupdf
+from autogen import register_function
+from autogen.agentchat import ChatResult, UserProxyAgent
+from autogen.agentchat.contrib.multimodal_conversable_agent import MultimodalConversableAgent
+from functools import partial
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from typing import Dict, List, Optional
+
+
+class PdfTriageWrapper(object):
+
+    def __init__(self, config_list: List[Dict], pdf_path: Optional[str]) -> None:
+        self.pdf_path = pdf_path
+
+        self.user_proxy = UserProxyAgent(
+            name="user_proxy",
+            human_input_mode="NEVER",
+            max_consecutive_auto_reply=0,
+            llm_config={"cache_seed": None, "temperature": 0, "config_list": config_list},
+        )
+        self.pdf_parser = MultimodalConversableAgent(
+            name="pdf_parser",
+            system_message="""
+            You can use the following functions to help you generate the answer:
+            
+            1. fetch_page(page_number) to retrieve the contents of a page from the PDF.
+            2. fetch_section(section) to retrieve the contents of a section from the PDF.
+            3. retrieve(query) to answer a general question about the PDF contents.
+            """,
+            llm_config={"cache_seed": None, "temperature": 0, "config_list": config_list},
+        )
+
+        openai.api_key = config_list[0]["api_key"]
+        openai.api_version = config_list[0]["model"]
+
+        vector_index = self.get_vector_index(pdf_path)
+
+        register_function(
+            partial(self.fetch_page, pdf_path),
+            caller=self.pdf_parser,
+            executor=self.user_proxy,
+            name="fetch_page",
+            description="Execute fetch_page(page_number) to retrieve the contents of a page.",
+        )
+        register_function(
+            partial(self.fetch_section, vector_index),
+            caller=self.pdf_parser,
+            executor=self.user_proxy,
+            name="fetch_section",
+            description="Execute fetch_section(section) to retrieve the contents of a section.",
+        )
+        register_function(
+            partial(self.retrieve, vector_index),
+            caller=self.pdf_parser,
+            executor=self.user_proxy,
+            name="retrieve",
+            description="Execute retrieve(query) to retrieve an answer for a given query.",
+        )
+
+    @staticmethod
+    def get_vector_index(pdf_path: Optional[str]) -> VectorStoreIndex:
+        index_documents = SimpleDirectoryReader(
+            input_files=([pdf_path] if pdf_path is not None else [])
+        ).load_data()
+        return VectorStoreIndex.from_documents(index_documents)
+
+    @staticmethod
+    def fetch_page(pdf_path: Optional[str], page_number: int) -> str:
+        if pdf_path is None:
+            return ""
+
+        document = pymupdf.open(pdf_path)
+        page = document.load_page(page_number - 1)
+        return page.get_text()
+
+    @staticmethod
+    def query_index(vector_index: VectorStoreIndex, query: str) -> str:
+        query_engine = vector_index.as_query_engine()
+        response = query_engine.query(query)
+        return response.response_txt
+
+    @staticmethod
+    def fetch_section(vector_index: VectorStoreIndex, section: str) -> str:
+        return PdfTriageWrapper.query_index(
+            vector_index,
+            f"Get me the contexts of the section {section}.",
+        )
+
+    @staticmethod
+    def retrieve(vector_index: VectorStoreIndex, query: str) -> str:
+        return PdfTriageWrapper.query_index(
+            vector_index,
+            f"Provide the most relevant context for the following query: \"{query}\"",
+        )
+
+    def initiate_chat(self, user_prompt: str) -> ChatResult:
+        self.pdf_parser.reset()
+
+        prompt = user_prompt if self.pdf_path is None else f"""
+            Using the attached PDF, answer the following question:
+
+            {user_prompt}
+            """
+
+        return self.user_proxy.initiate_chat(
+            self.pdf_parser,
+            message=prompt,
+        )
